584b027fb01323967b0d16cc672c181dd7295326
Rafael David Tinoco <rafaeldtinoco@ubuntu.com>
2020-03-15 14:42:18 +0000

    moved from other repo

---
+++ scripts/crontab.sh
@@ -0,0 +1,5 @@
+/home/rafaeldtinoco/work/sources/upstream/.gitupdate.sh
+/home/rafaeldtinoco/work/sources/ubuntu/.gitupdate.sh
+/home/rafaeldtinoco/work/sources/debian/.gitupdate.sh
+/home/rafaeldtinoco/work/sources/debian-mine/.gitupdate.sh
+/home/rafaeldtinoco/work/sources/kernel/.gitupdate.sh

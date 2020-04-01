c044b46c2ec192a3e11c8efad52522694ffcc696
Rafael David Tinoco <rafaeldtinoco@ubuntu.com>
2020-04-01 11:21:55 -0300

    nothing to see here

--- scripts/crontab.sh
+++
@@ -1,5 +0,0 @@
-/home/rafaeldtinoco/work/sources/upstream/.gitupdate.sh
-/home/rafaeldtinoco/work/sources/ubuntu/.gitupdate.sh
-/home/rafaeldtinoco/work/sources/debian/.gitupdate.sh
-/home/rafaeldtinoco/work/sources/debian-mine/.gitupdate.sh
-/home/rafaeldtinoco/work/sources/kernel/.gitupdate.sh

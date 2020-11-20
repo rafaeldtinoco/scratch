6caacdc7c29d1e5a7a151d569049eda0e7dc67f0
Rafael David Tinoco <rafaeldtinoco@ubuntu.com>
2020-11-20 19:59:47 +0000

    conntracker update

--- conntracker/conntracker.c
+++ conntracker/conntracker.c
@@ -47,32 +47,36 @@ gchar *ipv6_str(struct in6_addr *addr)
 GSequence *tcpv4flows;
 GSequence *udpv4flows;
 GSequence *icmpv4flows;
-
 GSequence *tcpv6flows;
 GSequence *udpv6flows;
 GSequence *icmpv6flows;
 
+void cleanflow(gpointer data)
+{
+	g_free(data);
+}
+
 /* functions used to sort existing sequences based on its elements */
 
-#define cmpbase(type, arg1, arg2)						\
-gint cmp_##type(struct type one, struct type two)				\
-{										\
-	if (one.arg1 < two.arg1)						\
-		return LESS;							\
-										\
-	if (one.arg1 > two.arg1)						\
-		return MORE;							\
-										\
-	if (one.arg1 == two.arg1)						\
-	{									\
-		if (one.arg2 < two.arg2)					\
-			return LESS;						\
-										\
-		if (one.arg2 > two.arg2)					\
-			return MORE;						\
-	}									\
-										\
-	return EQUAL;								\
+#define cmpbase(type, arg1, arg2)								\
+gint cmp_##type(struct type one, struct type two)						\
+{												\
+	if (one.arg1 < two.arg1)								\
+		return LESS;									\
+												\
+	if (one.arg1 > two.arg1)								\
+		return MORE;									\
+												\
+	if (one.arg1 == two.arg1)								\
+	{											\
+		if (one.arg2 < two.arg2)							\
+			return LESS;								\
+												\
+		if (one.arg2 > two.arg2)							\
+			return MORE;								\
+	}											\
+												\
+	return EQUAL;										\
 }
 
 cmpbase(ipv4base, src.s_addr, dst.s_addr);
@@ -120,22 +124,22 @@ int cmp_ipv6base(struct ipv6base one, struct ipv6base two)
 
 /* call proper comparison/sorting functions based on given type */
 
-#define cmpflow(type, arg1, arg2)						\
-int cmp_##type(struct type *one, struct type *two)				\
-{										\
-	int res;								\
-										\
-	if ((res = cmp_##arg1(one->addrs, two->addrs)) != EQUAL)		\
-		return res;							\
-	if ((res = cmp_##arg2(one->base, two->base)) != EQUAL)			\
-		return res;							\
-										\
-	if (one->reply < two->reply)						\
-		return LESS;							\
-	if (one->reply > two->reply)						\
-		return MORE;							\
-										\
-	return EQUAL;								\
+#define cmpflow(type, arg1, arg2)								\
+int cmp_##type(struct type *one, struct type *two)						\
+{												\
+	int res;										\
+												\
+	if ((res = cmp_##arg1(one->addrs, two->addrs)) != EQUAL)				\
+		return res;									\
+	if ((res = cmp_##arg2(one->base, two->base)) != EQUAL)					\
+		return res;									\
+												\
+	if (one->reply < two->reply)								\
+		return LESS;									\
+	if (one->reply > two->reply)								\
+		return MORE;									\
+												\
+	return EQUAL;										\
 }
 
 cmpflow(tcpv4flow, ipv4base, portbase);
@@ -147,15 +151,15 @@ cmpflow(icmpv6flow, ipv6base, icmpbase);
 
 /* compare two given flows (tcpv4, udpv4, icmpv4, tcpv6, udpv6 or icmpv6) */
 
-#define cmpflows(type)								\
-gint cmp_##type##s(gconstpointer ptr_one,					\
-		   gconstpointer ptr_two,					\
-		   gpointer data)						\
-{										\
-	struct type *one = (struct type *) ptr_one;				\
-	struct type *two = (struct type *) ptr_two;				\
-										\
-	return cmp_##type(one, two);						\
+#define cmpflows(type)										\
+gint cmp_##type##s(gconstpointer ptr_one,							\
+		   gconstpointer ptr_two,							\
+		   gpointer data)								\
+{												\
+	struct type *one = (struct type *) ptr_one;						\
+	struct type *two = (struct type *) ptr_two;						\
+												\
+	return cmp_##type(one, two);								\
 }
 
 cmpflows(tcpv4flow);
@@ -167,18 +171,45 @@ cmpflows(icmpv6flow);
 
 /* add flows based on given type */
 
-#define addflows(type)								\
-gint add_##type##s(struct type *flow)						\
-{										\
-	gpointer gptr;								\
-										\
-	gptr = g_malloc0(sizeof(struct type));					\
-	memcpy(gptr, flow, sizeof(struct type));				\
-										\
-	if (g_sequence_lookup(type##s, gptr, cmp_##type##s, NULL) == NULL)	\
-		g_sequence_insert_sorted (type##s, gptr, cmp_##type##s, NULL);	\
-										\
-	return SUCCESS;								\
+#define addflows(type)										\
+gint add_##type##s(struct type *flow)								\
+{												\
+	struct type *temp;									\
+	GSequenceIter *found, *found2;								\
+												\
+	temp = g_malloc0(sizeof(struct type));							\
+	memcpy(temp, flow, sizeof(struct type));						\
+												\
+	found = g_sequence_lookup(type##s, temp, cmp_##type##s, NULL);				\
+												\
+	if (found == NULL) { 									\
+		switch (temp->reply) {								\
+		case 0:										\
+			temp->reply = 1;							\
+			found2 = g_sequence_lookup(type##s, temp, cmp_##type##s, NULL); 	\
+			temp->reply = 0;							\
+			if (found2 == NULL) {							\
+				g_sequence_insert_sorted(type##s, temp, cmp_##type##s, NULL);	\
+				goto inserted;							\
+			}									\
+			break;									\
+		case 1:										\
+			temp->reply = 0;							\
+			found2 = g_sequence_lookup(type##s, temp, cmp_##type##s, NULL); 	\
+			temp->reply = 1;							\
+			if (found2 != NULL) {							\
+				g_sequence_remove(found2);					\
+				g_sequence_insert_sorted(type##s, temp, cmp_##type##s, NULL);	\
+				goto inserted;							\
+			}									\
+			break;									\
+		}										\
+	}											\
+												\
+	g_free(temp); 										\
+												\
+inserted:											\
+	return SUCCESS;										\
 }
 
 addflows(tcpv4flow);
@@ -190,22 +221,22 @@ addflows(icmpv6flow);
 
 /* call addflows */
 
-#define addflow(arg1, arg2, arg3, arg4)						\
-gint add##arg1(struct arg2 s, struct arg2 d,					\
-		uint16_t ps, uint16_t pd, uint8_t r)				\
-{										\
-	struct arg1 flow;							\
-	memset(&flow, '0', sizeof(struct arg1));				\
-										\
-	flow.addrs.src = s;							\
-	flow.addrs.dst = d;							\
-	flow.base.arg3 = ps;							\
-	flow.base.arg4 = pd;							\
-	flow.reply = r;								\
-										\
-	add_##arg1##s(&flow);							\
-										\
-	return SUCCESS;								\
+#define addflow(arg1, arg2, arg3, arg4)								\
+gint add##arg1(struct arg2 s, struct arg2 d,							\
+		uint16_t ps, uint16_t pd, uint8_t r)						\
+{												\
+	struct arg1 flow;									\
+	memset(&flow, '0', sizeof(struct arg1));						\
+												\
+	flow.addrs.src = s;									\
+	flow.addrs.dst = d;									\
+	flow.base.arg3 = ps;									\
+	flow.base.arg4 = pd;									\
+	flow.reply = r;										\
+												\
+	add_##arg1##s(&flow);									\
+												\
+	return SUCCESS;										\
 }
 
 addflow(tcpv4flow, in_addr, src, dst);
@@ -218,52 +249,44 @@ addflow(icmpv6flow, in6_addr, type, code);
 /* display the flows */
 
 #define printa(arg1, arg2, ...) \
-void printa_##arg1##s(gpointer data, gpointer user_data)			\
-{										\
-	static int times = 0;							\
-	gchar *src, *dst;							\
-	struct arg1 *flow = data;						\
-										\
-	src = arg2##_str(&flow->addrs.src);					\
-	dst = arg2##_str(&flow->addrs.dst);					\
-										\
-	printf(__VA_ARGS__);							\
-										\
-	g_free(src);								\
-	g_free(dst);								\
+void printa_##arg1##s(gpointer data, gpointer user_data)					\
+{												\
+	static int times = 0;									\
+	gchar *src, *dst;									\
+	struct arg1 *flow = data;								\
+												\
+	src = arg2##_str(&flow->addrs.src);							\
+	dst = arg2##_str(&flow->addrs.dst);							\
+												\
+	printf(__VA_ARGS__);									\
+												\
+	g_free(src);										\
+	g_free(dst);										\
 }
 
-printa(tcpv4flow, ipv4, "[%12d]  TCPv4 src = %s (port=%u) to dst = %s (port=%u)%s\n", times++,
-		src, (int) ntohs(flow->base.src),
-		dst, (int) ntohs(flow->base.dst),
-		flow->reply ? " (R)" : "");
+printa(tcpv4flow, ipv4, " TCPv4 [%12d] src = %s (port=%u) to dst = %s (port=%u)%s\n", times++,
+		src, ntohs(flow->base.src), dst, ntohs(flow->base.dst),
+		flow->reply ? " (confirmed)" : "");
 
-printa(udpv4flow, ipv4, "[%12d]  UDPv4 src = %s (port=%u) to dst = %s (port=%u)%s\n", times++,
-		src, (int) ntohs(flow->base.src),
-		dst, (int) ntohs(flow->base.dst),
-		flow->reply ? " (R)" : "");
+printa(udpv4flow, ipv4, " UDPv4 [%12d] src = %s (port=%u) to dst = %s (port=%u)%s\n", times++,
+		src, ntohs(flow->base.src), dst, ntohs(flow->base.dst),
+		flow->reply ? " (confirmed)" : "");
 
-printa(icmpv4flow, ipv4, "[%12d] ICMPv4 src = %s to dst = %s (type=%u | code=%u)%s\n", times++,
-		src, dst,
-		(int) ntohs(flow->base.type),
-		(int) ntohs(flow->base.code),
-		flow->reply ? " (R)" : "");
+printa(icmpv4flow, ipv4, "ICMPv4 [%12d] src = %s to dst = %s (type=%u | code=%u)%s\n", times++,
+		src, dst, (uint8_t) ntohs(flow->base.type), (uint8_t) ntohs(flow->base.code),
+		flow->reply ? " (confirmed)" : "");
 
-printa(tcpv6flow, ipv6, "[%12d]  TCPv6  src = %s (port=%u) to dst = %s (port=%u)%s\n", times++,
-		src, (int) ntohs(flow->base.src),
-		dst, (int) ntohs(flow->base.dst),
-		flow->reply ? " (R)" : "");
+printa(tcpv6flow, ipv6, " TCPv6 [%12d] src = %s (port=%u) to dst = %s (port=%u)%s\n", times++,
+		src, ntohs(flow->base.src), dst, ntohs(flow->base.dst),
+		flow->reply ? " (confirmed)" : "");
 
-printa(udpv6flow, ipv6, "[%12d]  UDPv6 src = %s (port=%u) to dst = %s (port=%u)%s\n", times++,
-		src, (int) ntohs(flow->base.src),
-		dst, (int) ntohs(flow->base.dst),
-		flow->reply ? " (R)" : "");
+printa(udpv6flow, ipv6, " UDPv6 [%12d] src = %s (port=%u) to dst = %s (port=%u)%s\n", times++,
+		src, ntohs(flow->base.src), dst, ntohs(flow->base.dst),
+		flow->reply ? " (confirmed)" : "");
 
-printa(icmpv6flow, ipv6, "[%12d] ICMPv6 src = %s to dst = %s (type=%u | code=%u)%s\n", times++,
-		src, dst,
-		(int) ntohs(flow->base.type),
-		(int) ntohs(flow->base.code),
-		flow->reply ? " (R)" : "");
+printa(icmpv6flow, ipv6, "ICMPv6 [%12d] src = %s to dst = %s (type=%u | code=%u)%s\n", times++,
+		src, dst, (uint8_t) ntohs(flow->base.type), (uint8_t) ntohs(flow->base.code),
+		flow->reply ? " (confirmed)" : "");
 
 /* debug */
 
@@ -284,13 +307,13 @@ static int event_cb(enum nf_conntrack_msg_type type,
 {
 	short reply = 0;
 
-	const uint8_t *family = NULL, *proto = NULL;
-	const uint16_t *port_src = NULL, *port_dst = NULL;
-	const uint8_t *itype = NULL, *icode = NULL;
-	const uint32_t *constatus = NULL;
-	struct in6_addr *ipv6_src_in = NULL, *ipv6_dst_in = NULL;
-
+	uint8_t *family = NULL, *proto = NULL;
+	uint16_t *port_src = NULL, *port_dst = NULL;
+	uint8_t *itype = NULL, *icode = NULL;
+	uint32_t *constatus = NULL;
 	struct in_addr ipv4_src_in, ipv4_dst_in;
+	struct in6_addr *ipv6_src_in = NULL, *ipv6_dst_in = NULL;
+	uint16_t privport = htons(1024);
 
 	/* initialize to avoid compiler warnings */
 
@@ -352,6 +375,9 @@ static int event_cb(enum nf_conntrack_msg_type type,
 	case IPPROTO_UDP:
 		port_src = (uint16_t *) nfct_get_attr(ct, ATTR_PORT_SRC);
 		port_dst = (uint16_t *) nfct_get_attr(ct, ATTR_PORT_DST);
+		/* all unprivileged source ports logged as 1024 */
+		if ((int) ntohs(*port_src) > 1024)
+			port_src = &privport;
 		break;
 	case IPPROTO_ICMP:
 	case IPPROTO_ICMPV6:
@@ -394,7 +420,6 @@ static int event_cb(enum nf_conntrack_msg_type type,
 	return NFCT_CB_CONTINUE;
 }
 
-
 // cleanup
 
 void cleanup(void)
@@ -402,7 +427,6 @@ void cleanup(void)
 	g_sequence_foreach(tcpv4flows, printa_tcpv4flows, NULL);
 	g_sequence_foreach(udpv4flows, printa_udpv4flows, NULL);
 	g_sequence_foreach(icmpv4flows, printa_icmpv4flows, NULL);
-
 	g_sequence_foreach(tcpv6flows, printa_tcpv6flows, NULL);
 	g_sequence_foreach(udpv6flows, printa_udpv6flows, NULL);
 	g_sequence_foreach(icmpv6flows, printa_icmpv6flows, NULL);
@@ -410,7 +434,6 @@ void cleanup(void)
 	g_sequence_free(tcpv4flows);
 	g_sequence_free(udpv4flows);
 	g_sequence_free(icmpv4flows);
-
 	g_sequence_free(tcpv6flows);
 	g_sequence_free(udpv6flows);
 	g_sequence_free(icmpv6flows);
@@ -419,7 +442,6 @@ void cleanup(void)
 void handler(int what)
 {
 	cleanup();
-
 	exit(SUCCESS);
 }
 
@@ -432,12 +454,12 @@ int main(void)
 
 	signal(SIGINT, handler);
 
-	tcpv4flows = g_sequence_new(NULL); // TODO: create a destroy function to free items
-	udpv4flows = g_sequence_new(NULL);
-	icmpv4flows = g_sequence_new(NULL);
-	tcpv6flows = g_sequence_new(NULL);
-	udpv6flows = g_sequence_new(NULL);
-	icmpv6flows = g_sequence_new(NULL);
+	tcpv4flows = g_sequence_new(cleanflow);
+	udpv4flows = g_sequence_new(cleanflow);
+	icmpv4flows = g_sequence_new(cleanflow);
+	tcpv6flows = g_sequence_new(cleanflow);
+	udpv6flows = g_sequence_new(cleanflow);
+	icmpv6flows = g_sequence_new(cleanflow);
 
 	h = nfct_open(CONNTRACK, NF_NETLINK_CONNTRACK_NEW | NF_NETLINK_CONNTRACK_UPDATE);
 	if (!h) {
@@ -455,7 +477,6 @@ int main(void)
 endclean:
 
 	cleanup();
-
 	exit(ret);
 }
 
